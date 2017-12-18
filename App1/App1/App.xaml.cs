6614ec78a413f01fe7adb065b8cc485d7e7c0506
Jorge Martins <jorge.martins@saipher.com.br>
2017-12-18 14:03:56 -0200

    Add project files.

---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using App1.Views;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class App : Application
+	{
+
+		public App ()
+		{
+			InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+		protected override void OnStart ()
+		{
+            AppCenter.Start("ios=0e5f0076-b805-457f-a4ea-aa42f9a2cb2a;" + "uwp={Your UWP App secret here};" +
+                   "android={Your Android App secret here}",
+                   typeof(Analytics), typeof(Crashes));
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
